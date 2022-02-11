f0c179921a7e8c0057ba92124785a5d4e26c308a
DawonJeon <dawon3024@naver.com>
2022-02-11 10:57:28 +0900

    Docs: my_page.js변수설정 및 firstinit함수 실행

--- src/js/new/user/my_page.js
+++ src/js/new/user/my_page.js
@@ -1,1 +1,32 @@
-console.log('hello')
+
+// 변수 설정
+const _userPicture =document.querySelector(".my_page_user_pic__personal-picture");
+const _userName = document.querySelector(".my_page_user-info__current-name");
+const _changeCampus = document.querySelector(".my_page_change_campus");
+const _changeProcess = document.querySelector(".my_page_change_process");
+
+
+
+
+
+
+
+
+// API
+
+const page_url = `https://d0ab52b3-193e-4494-a62f-48f4a76231e4.mock.pstmn.io/`
+
+
+
+const firstinit = (e) => {
+    fetch(page_url)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        _userName.placeholder = data[0].nickname;
+
+    })
+    .catch((err) => console.log(err));
+}
+
+firstinit();
