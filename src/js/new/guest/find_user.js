2e8617cab14649724ae9d9a02a7194a43ab04483
D.seok <82019373+Dseok12@users.noreply.github.com>
2022-02-22 16:31:07 +0900

    Test: login.js, join.js .env 파일 적용 및 login.html, find_user.html a링크 수정 (#59)
    * Test: login.js, join.js .env 파일 적용 및 login.html, find_user.html a링크 수정

--- src/js/new/guest/find_user.js
+++ src/js/new/guest/find_user.js
@@ -4,27 +4,31 @@ const _get = (target) => {
 }
 
 // ID 찾기 form DOM 들고오기
-const findIdForm = _get('.find-id')
-const findIdFormInput = _get('.find_user_find-id__id-phone-input')
-const findIdFormBtn = _get('.find_user_find-id__id-phone-btn')
+const _findIdForm = _get('.find-id')
+const _findIdFormInput = _get('.find_user_find-id__id-phone-input')
+const _findIdFormBtn = _get('.find_user_find-id__id-phone-btn')
 
 // password 찾기 form DOM 들고오기
-const findPwForm = _get('.find-pw')
+const _findPwForm = _get('.find-pw')
 
-const findPwFormPhoneInput = _get('.find_user_find-pw__pw-phone-input')
-const findPwFormPhoneBtn = _get('.find_user_find-pw__pw-phone-btn')
+const _findPwFormPhoneInput = _get('.find_user_find-pw__pw-phone-input')
+const _findPwFormPhoneBtn = _get('.find_user_find-pw__pw-phone-btn')
 
-const findPwFormCodeInput = _get('.find_user_find-pw__pw-code-input')
-const findPwFormCodeBtn = _get('.find_user_find-pw__pw-code-btn')
+const _findPwFormCodeInput = _get('.find_user_find-pw__pw-code-input')
+const _findPwFormCodeBtn = _get('.find_user_find-pw__pw-code-btn')
 
-const findPwFormResetInput = _get('.find_user_find-pw__pw-reset-input')
-const findPwFormResetBtn = _get('.find_user_find-pw__pw-reset-btn')
-
-
+const _findPwFormResetInput = _get('.find_user_find-pw__pw-reset-input')
+const _findPwFormResetBtn = _get('.find_user_find-pw__pw-reset-btn')
 
 
 
+const _findId = () => {
+  
+}
 
+const _bindIdFIndBtn = () => {
+  _findIdForm.addEventListener('submit', _findId)
+}
 
 const AllFindUser = () => {
 
